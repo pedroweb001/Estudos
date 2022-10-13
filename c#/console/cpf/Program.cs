acc34f22c338e8d9916f8ac4eb8d9c77aeee760b
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-13 12:52:59 -0300

    Formatação de cpf

---
+++ c#/console/cpf/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace cpf
+{
+    class Program
+    {
+        static void Main()
+        {
+            double cpf;
+            Console.WriteLine("Digite seu cpf:");
+            cpf = double.Parse(Console.ReadLine());
+            Console.WriteLine("O seu cpf é:" + string.Format(@"{0:000\.000\.000\-00}", cpf));
+            Console.WriteLine("O seu cpf é:" + cpf);
+        }
+    }
+}
