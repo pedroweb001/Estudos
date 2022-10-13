410a955bc1e26e3ad9ae07fbefd8d3667428c9c0
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-13 12:58:15 -0300

    Exemplos com try/catch

---
+++ c#/console/erros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace erro
+{
+    class Program
+    {
+        static void Main()
+        {
+            int v1, v2, v3;
+            try
+            {
+                Console.WriteLine("Digite o primeiro número");
+                v1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segundo número:");
+                v2 = int.Parse(Console.ReadLine());
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Erro. Digite somente números!");
+                Main();
+                return;
+                throw;
+            }
+            v3 = (v1+v2);
+            Console.WriteLine("O resultado da soma é:" + v3);
+        }
+    }
+}
