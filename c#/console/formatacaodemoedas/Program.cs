e499b50057f9d4554ac166a653b25673d885c716
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-13 12:59:09 -0300

    Formatando moedas.

---
+++ c#/console/formatacaodemoedas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace formatacaodemoedas
+{
+    class Program
+    {
+        static void Main()
+        {
+  double valor = 5.00;
+  NumberFormatInfo seila = new CultureInfo("pt-BR", false).NumberFormat;
+Console.WriteLine(valor.ToString("c", seila));
+        }
+    }
+}
