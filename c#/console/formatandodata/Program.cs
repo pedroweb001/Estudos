9d90ba3a12cecce981b73a06d029f156370336f0
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-13 12:59:59 -0300

    Formatando datas.

---
+++ c#/console/formatandodata/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace formatandodata
+{
+    class Program
+    {
+        static void Main()
+        {
+ var dataatual = DateTime.Now;           
+ CultureInfo ci = new CultureInfo("pt-BR");
+ Console.WriteLine("A data atual é:" + string.Format("{0:d}", dataatual.ToString(ci)));
+        }
+    }
+}
