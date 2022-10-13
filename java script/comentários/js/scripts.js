a0c2e1edcd8621ae6c992cea552996569c3a1546
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-13 13:06:41 -0300

    Comentários

---
+++ java script/comentários/js/scripts.js
@@ -0,0 +1,6 @@
+/*
+Esse é um script para testar os comentários dentro do código.
+Fica de exemplo pra você que deseja aprender esse conceito.
+Linhas comentadas, são ignoradas pelo interpretador java script, ou seja, quando chega nessa linha
+o interpretador as ignoram e executam o código normalmente.
+*/
