75281f853591eae36221e388d7b578f56dec6d7b
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-14 10:04:47 -0300

    Descobrindo os tipos de variáveis

---
+++ java script/typeof/js/script.js
@@ -0,0 +1,15 @@
+var nome = "pedro ian", idade = 19, preco_produto = 9.99;
+var array = ["pedro", 19];
+var objetos = {"nome":"pedro ian", idade:19,
+notificacao: function(){
+console.log("Ok");
+}
+};
+console.log(typeof(nome));
+console.log(typeof(idade));
+console.log(typeof(preco_produto));
+console.log(typeof(array));
+console.log(typeof(objetos));
+console.log(array[0]);
+console.log(objetos.nome);
+console.log(objetos.notificacao());
