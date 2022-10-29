87540fabfa334829576e2b3011c5dba9946ae370
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-29 15:48:50 -0300

    Transformação de strings em números e números em estrings

---
+++ java script/transformando_string_em_numero_e_numero_em_string/js/script.js
@@ -0,0 +1,8 @@
+teste();
+
+function teste()
+{
+let numero = "1";
+console.log(Number(numero));//Transformei essa string em um número
+console.log(String(numero));//Transformei esse número agora em uma string novamente.
+}
