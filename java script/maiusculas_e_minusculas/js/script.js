47624ef54cc0c0e66ffaf5cc4359a4c775a565b1
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-31 11:24:59 -0300

    Esse script pega tudo que o usuário digita, transforma o que o mesmo digitou em maiúscula e em minúscula através da função toupercase e tolowercase

---
+++ java script/maiusculas_e_minusculas/js/script.js
@@ -0,0 +1,7 @@
+transforms();
+
+function transforms(){
+    let nome = prompt("Digite seu nome:");
+    console.log(nome.toUpperCase());//Transformando tudo que o usuário digita e mmaiúscula.
+console.log(nome.toLowerCase());//Agora fazendo o oposto, pegando tudo que o usuário digita e transformando em minúscula.
+}
