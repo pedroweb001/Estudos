a8c1b4c8209c66cd607b509d05e5fd6a32c9b65d
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-10-31 13:38:40 -0300

    Nesse pequeno script, verifico se uma palavra existe em uma determinada frase

---
+++ java script/encontrando_palavras_em_frases/js/script.js
@@ -0,0 +1,6 @@
+encontrando_palavras_em_frase();
+function encontrando_palavras_em_frase(){
+    let frase = "Eu te amo demais!";
+    console.log(frase.includes("demais"));
+    console.log(frase.includes("ola"));
+}
