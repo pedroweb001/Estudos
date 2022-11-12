c19fe9685b7e321bcfee1be43e8fc905705e7fd6
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-11-11 21:31:22 -0300

    At1 algoritmo uni santos

---
+++ python/exercícios/ex01.py
@@ -0,0 +1,31 @@
+#encoding: utf-8
+current = True
+n1 = int(input("Digite o primeiro bit"))
+n2 = int(input("Digite o segundo bit"))
+n3 = int(input("Digite o terceiro bit"))
+n4 = int(input("Digite o quarto bit"))
+n5 = int(input("Digite o quinto bit"))
+n6 = int(input("Digite o sexto bit"))
+n7 = int(input("Digite o sétimo bit"))
+n8 = int(input("Digite o oitavo bit"))
+if (n1>1):
+    current = False
+elif (n2>1):
+    current = False
+elif (n3>1):
+    current = False
+elif (n4>1):
+    current = False
+elif (n5>1):
+    current = False
+elif(n6>1):
+    current = False
+elif (n7>1):
+    current = false
+elif (n8>1):
+ current = False       
+ 
+if (current):
+ print("s")     
+else:
+ print("f")   
