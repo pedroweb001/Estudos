b57b5b1d821961e39a28c299f983644878e104f9
Pedro Ian Brito da Silva <pedroianbritodasilva@gmail.com>
2022-11-18 14:19:03 -0300

    Meu primeiro pequeno projeto com node ejs. Estou adorando!

---
+++ node_ejs/prj_ex_01/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+app.get('/', function(req, res){
+res.render('./index');
+});
+
+app.get('/sobre', function(req, res){
+    res.render('./sobre');
+    });
+app.listen(8080);
+console.log("Rodando");
