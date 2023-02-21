614d4dbf6ae617b8ce2e3396138f0deb94c2674c
Pedro Ian <71662757+pedroweb001@users.noreply.github.com>
2023-02-21 20:11:20 -0300

    Delete conectando_no_banco_de_dados.py

--- python/conectando_no_banco_de_dados.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector    
-conexao = mysql.connector.Connect(host="localhost", user="root", password="root", database="prj_teste")
-if (conexao.is_connected):
-    info = conexao.get_server_info()
-    print("Você está conectado ao My Sql versão:", info)
-    conexao.close()
-else:
-    print("Não consegui conectar!")    
-    exit()
