057c638a418b0408e8a4cce813e0157cbb4a4397
pedro ian <pedroianbritodasilva@gmail.com>
2023-04-18 19:33:00 -0300

    Adicionado o método delete na aplicação.

--- node_js/apis/node_api_restful/api/controlers/customerWallets.js
+++ node_js/apis/node_api_restful/api/controlers/customerWallets.js
@@ -1,28 +1,50 @@
 const uuid = require("uuid/v4");
 
-module.exports = ()=>{
+module.exports = () => {
     const customerWalletsDB = require("../data/customer-wallets.json");
     const controller = {}
 
-    const {customerWallets:customerWalletsMock} = customerWalletsDB;
+    const { customerWallets: customerWalletsMock } = customerWalletsDB;
 
-    controller.listCustomerWallets = (req, res)=>{
+    controller.listCustomerWallets = (req, res) => {
         res.status(200).json(customerWalletsDB);
-    } 
+    }
 
-    controller.saveCustomerWallets = (req, res)=>{
+    controller.saveCustomerWallets = (req, res) => {
         customerWalletsMock.data.push({
-            id:uuid(),
-            name:req.body.name,
-            birthDate:req.body.birthDate,
-            parentId:uuid(),
-            email:req.body.email,
-            occupation:req.body.occupation,
-            cellphone:req.body.cellphone,
-            phone:req.body.phone,
-            state:req.body.state
+            id: uuid(),
+            name: req.body.name,
+            birthDate: req.body.birthDate,
+            parentId: uuid(),
+            email: req.body.email,
+            occupation: req.body.occupation,
+            cellphone: req.body.cellphone,
+            phone: req.body.phone,
+            state: req.body.state
         });
         res.status(201).json(customerWalletsMock);
+    }
+
+    controller.removeCustomerWallets = (req, res) => {
+        const { id } = req.params;
+
+        const foundCustomerIndex = customerWalletsMock.data.findIndex(customer => customer.id == id);
+
+        if (foundCustomerIndex == -1) {
+            res.status(404).json({
+                message: "Cliente não encontrado.",
+                success: false,
+                customerWallets: customerWalletsMock,
+            });
+        }
+        else {
+            customerWalletsMock.data.splice(foundCustomerIndex, 1);
+            res.status(200).json({
+                message: "Cliente removido com sucesso!",
+                sucess: true,
+                customerWallets: customerWalletsMock,
+            });
+        }
     }
     return controller;
 }
