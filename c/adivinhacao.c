cdad0915beff649ed46891300b33e06ab1a85d6b
unknown <pedroianbritodasilva@gmail.com>
2023-06-11 04:17:55 -0300

    Joguinho básico a onde o computador sorteia o número. E o jogador tem que adivinhar qual é

---
+++ c/adivinhacao.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+int main(){
+setlocale(LC_ALL,"Portuguese");
+double pontos = 1000;
+srand(time(0));
+int numerosecreto = rand()%100;
+int acertou = 0;
+int nivel;
+int totaldetentativas;
+printf("************************************\n");
+printf("*Bem-vindo ao jogo de adivinhação*\n");
+printf("************************************\n");
+printf("Qual é o nível de dificuldade?\n");
+printf("1 - fácil\n");
+printf("2 - médio\n");
+printf("3 - difícil\n");
+printf("Escolha:");
+scanf("%d", &nivel);
+switch(nivel){
+case 1:
+totaldetentativas = 20;
+break;
+case 2:
+totaldetentativas = 10;
+break;
+case 3:
+totaldetentativas = 5;
+break;
+default:
+printf("Opção inválida!\n");
+printf("Qual é o nível de dificuldade?\n");
+printf("1 - fácil\n");
+printf("2 - médio\n");
+printf("3 - difícil\n");
+printf("Escolha:");
+}
+int chute;
+for (int contador = 1; contador<=totaldetentativas; contador++){ 
+printf("Qual é seu %do. Chute?", contador);
+scanf("%d", &chute);
+if (chute<0){
+printf("Você não pode chutar números negativos!\n");
+continue;
+}
+printf("Seu %do. Chute foi %d\n", contador, chute);
+int acertou = chute == numerosecreto;
+int maior = chute == numerosecreto;
+int menor = chute == numerosecreto;
+if (acertou){
+break;
+}
+if (menor){
+printf("O número digitado é menor que o número secreto!\n");
+}
+if (menor){
+printf("O número digitado é menor que o número secreto!\n");
+}
+double pontosperdidos = abs(chute-numerosecreto)/2.0;
+pontos = pontosperdidos;
+}
+if (acertou){
+printf("Parabéns. Você acertou o número secreto!\n");
+printf("Você fez %.2f pontos \n", pontos);
+}
+else{
+printf("Você perdeu!");
+printf("Você fez %.2f pontos \n", pontos);
+}
+int resposta_jogador;
+printf("Você deseja jogar novamente? Tecle 1 para sim, 2 para não:");
+scanf("%d", &resposta_jogador);
+switch(resposta_jogador){
+case 1:
+main();
+break;
+case 2:
+printf("Obrigado por participar. Até a próxima!\n");
+exit(0);
+break;
+default:
+printf("Opção inválida!");
+break;
+}
+return 0;
+}
